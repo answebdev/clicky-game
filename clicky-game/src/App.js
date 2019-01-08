b15fe352c6d47f6058a188f4654685da2b518c61
ANS <36783010+answebdev@users.noreply.github.com>
2019-01-07 17:04:43 -0800

    added Navbar component

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
-import logo from './yin-yang.png';
+// import logo from './yin-yang.png';
 import './App.css';
+import Navbar from './components/Navbar';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Clicky Game coming soon!
+//           </p>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Clicky Game coming soon!
-          </p>
-        </header>
+      <div>
+        <Navbar />
+
       </div>
     );
   }
