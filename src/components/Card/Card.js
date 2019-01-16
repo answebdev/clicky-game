c932f945918011ad41040d32f3f137a937d79a06
ANS <36783010+answebdev@users.noreply.github.com>
2019-01-16 02:06:26 -0800

    cleaned up code

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,7 +6,6 @@ const Card = (props) => (
     <div className="container">
         <div className="card" onClick={() => props.clickHandler(props.id)}>
             <div className="img-container">
-                {/* <img src={bart} alt="Bart" /> */}
                 <img alt={props.name} src={props.image} />
             </div>
         </div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,13 +4,6 @@ import "./Footer.css";
 class Footer extends Component {
     render() {
         return (
-            // <footer className="page-footer">
-            //     <div className="footer-copyright">
-            //         <div className="container" id="footer-container">
-            //             The Simpsons Clicky Game
-            //         </div>
-            //     </div>
-            // </footer>
             <footer className="footer">
                 <div className="container">
                     <span id="footer-text">The Simpsons Clicky Game &nbsp;<i id="react" class="fab fa-react"></i></span>
